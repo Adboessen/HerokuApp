28e626f6d05c06c41b6f4dad70fcc0a1e0244844
Andrew <andrewboessen@amdg.rockhursths.edu>
2022-02-10 11:25:41 -0600

    Google OAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
+import GoogleLogin from "react-google-login";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,11 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  const responseGoogle = (response) => {
+    console.log(response.tokenId);
+    console.log(response.profileObj);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +25,17 @@ function App() {
 
         <p>{!data ? "Loading..." : data}</p>
       </header>
+      <nav>
+        <div>Test</div>
+        <GoogleLogin
+          clientId="1085897457627-09537qfb2nl4u3bosi9qoe5vlbagosk1.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        />
+      </nav>
     </div>
   );
 }
